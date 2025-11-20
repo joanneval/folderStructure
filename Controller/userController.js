f4d494005d23a7b614edcd36d39f8a4f0edafd4a
joann <evaljoann63@gmail.com>
2025-11-20 12:57:41 +0800

    structure

---
+++ Controller/userController.js
@@ -0,0 +1,21 @@
+
+import  {getUserById}  from "../Model/userModel.js";
+export const getAllUser = (req, res) => {
+    try {
+
+
+        const { id } = req.params;
+        const users = getUserById(id);
+
+        if (!users || users.length ===0 ){
+            return res.status(404).json({ message: "No users found" });
+        }
+
+        res.status(200).json(users);
+
+    } catch{
+
+        res.status(500).json({message: "Server Error"});
+
+    }
+}
---
+++ Model/userModel.js
@@ -0,0 +1,28 @@
+const users = [
+  { id: 1, name: "Alice", email: "alice@example.com" },
+  { id: 2, name: "Bob", email: "bob@example.com" },
+  { id: 3, name: "Charlie", email: "charlie@example.com" },
+  { id: 4, name: "David", email: "david@example.com" },
+  { id: 5, name: "Eva", email: "eva@example.com" },
+  { id: 6, name: "Frank", email: "frank@example.com" },
+  { id: 7, name: "Grace", email: "grace@example.com" },
+  { id: 8, name: "Henry", email: "henry@example.com" },
+  { id: 9, name: "Irene", email: "irene@example.com" },
+  { id: 10, name: "Jack", email: "jack@example.com" },
+  { id: 11, name: "Karen", email: "karen@example.com" },
+  { id: 12, name: "Liam", email: "liam@example.com" },
+  { id: 13, name: "Mia", email: "mia@example.com" },
+  { id: 14, name: "Noah", email: "noah@example.com" },
+  { id: 15, name: "Olivia", email: "olivia@example.com" },
+  { id: 16, name: "Paul", email: "paul@example.com" },
+  { id: 17, name: "Quinn", email: "quinn@example.com" },
+  { id: 18, name: "Rachel", email: "rachel@example.com" },
+  { id: 19, name: "Sam", email: "sam@example.com" },
+  { id: 20, name: "Tina", email: "tina@example.com" },
+];
+
+export const getUserById = (id) => {
+    return users.find(user => user.id === parseInt(id));
+};
+
+ 
